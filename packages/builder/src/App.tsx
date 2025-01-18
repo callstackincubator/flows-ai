069f0d8887385f915d968d4281125697d8903fa5
Kewin Wereszczynski <kewin.wereszczynski@callstack.com>
2025-01-18 18:50:34 +0100

    feat: added Dagre

--- packages/builder/src/App.tsx
+++ packages/builder/src/App.tsx
@@ -1,12 +1,51 @@
 import '@xyflow/react/dist/style.css'
 
-import { Background, Edge, Node, ReactFlow } from '@xyflow/react'
+import Dagre from '@dagrejs/dagre'
+import {
+  Background,
+  Edge,
+  Node,
+  Panel,
+  ReactFlow,
+  useEdgesState,
+  useNodesState,
+  useReactFlow,
+} from '@xyflow/react'
 import { FlowDefinition } from 'flows-ai'
-import { useMemo } from 'react'
+import { useCallback, useMemo } from 'react'
 
 // tbd: let's keep it during testing phase
 // going forward, we will have to do something such as drag&drop or loader
 import { githubProjectHealthAnalysisFlow } from '../../../example/github.ts'
+
+const getLayoutedElements = (nodes: Node[], edges: Edge[], direction: string) => {
+  const g = new Dagre.graphlib.Graph().setDefaultEdgeLabel(() => ({}))
+  g.setGraph({ rankdir: direction })
+
+  edges.forEach((edge) => g.setEdge(edge.source, edge.target))
+  nodes.forEach((node) =>
+    g.setNode(node.id, {
+      ...node,
+      width: node.measured?.width ?? 0,
+      height: node.measured?.height ?? 0,
+    })
+  )
+
+  Dagre.layout(g)
+
+  return {
+    nodes: nodes.map((node) => {
+      const position = g.node(node.id)
+      // We are shifting the dagre node position (anchor=center center) to the top left
+      // so it matches the React Flow node anchor point (top left).
+      const x = position.x - (node.measured?.width ?? 0) / 2
+      const y = position.y - (node.measured?.height ?? 0) / 2
+
+      return { ...node, position: { x, y } }
+    }),
+    edges,
+  }
+}
 
 /**
  * Recursively generates nodes and edges for a flow visualization.
@@ -19,18 +58,15 @@ function generateNodesAndEdges(
 ): { nodes: Node[]; edges: Edge[] } {
   const parentId = parent?.id || nodes.at(-1)?.id
 
-  const sameParentCount = nodes.filter((node) => node.parentId === parentId).length
-
   const currentNode: Node = {
     id: `${nodes.length}-${flow.agent}`,
     data: {
       label: flow.agent,
       agent: flow.agent,
     },
-    parentId,
     position: {
-      x: sameParentCount ? sameParentCount * 70 + 100 : 0,
-      y: parentId ? 70 : 0,
+      x: 0,
+      y: 0,
     },
   }
 
@@ -41,6 +77,7 @@ function generateNodesAndEdges(
       id: `${currentNode.id}-${parentId}`,
       source: parentId,
       target: currentNode.id,
+      animated: true,
     })
   }
 
@@ -80,14 +117,45 @@ function generateNodesAndEdges(
 }
 
 function Flow() {
-  const { nodes, edges } = useMemo(() => generateNodesAndEdges(githubProjectHealthAnalysisFlow), [])
+  const { fitView } = useReactFlow()
+
+  const { nodes: initialNodes, edges: initialEdges } = useMemo(
+    () => generateNodesAndEdges(githubProjectHealthAnalysisFlow),
+    []
+  )
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes)
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges)
+
+  const onLayout = useCallback(
+    (direction: string) => {
+      console.log(nodes)
+      const layouted = getLayoutedElements(nodes, edges, direction)
+
+      setNodes([...layouted.nodes])
+      setEdges([...layouted.edges])
+
+      window.requestAnimationFrame(() => {
+        fitView()
+      })
+    },
+    [nodes, edges]
+  )
 
   return (
-    <div style={{ height: '100vh', width: '100vw' }}>
-      <ReactFlow nodes={nodes} edges={edges}>
-        <Background />
-      </ReactFlow>
-    </div>
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      fitView
+    >
+      <Panel position="top-right">
+        <button onClick={() => onLayout('TB')}>vertical layout</button>
+        <button onClick={() => onLayout('LR')}>horizontal layout</button>
+      </Panel>
+      <Background />
+    </ReactFlow>
   )
 }
 
--- packages/builder/src/main.tsx
+++ packages/builder/src/main.tsx
@@ -1,5 +1,6 @@
 import './index.css'
 
+import { ReactFlowProvider } from '@xyflow/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
@@ -7,6 +8,10 @@ import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ReactFlowProvider>
+      <div style={{ width: '100vw', height: '100vh' }}>
+        <App />
+      </div>
+    </ReactFlowProvider>
   </StrictMode>
 )
