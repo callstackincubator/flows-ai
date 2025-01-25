19baaa92f2c8b1f0529a12a9a9820572dd42cf00
Kewin Wereszczynski <kewin.wereszczynski@callstack.com>
2025-01-25 14:26:24 +0100

    fix: small style changes

--- docs/src/components/Sandbox.tsx
+++ docs/src/components/Sandbox.tsx
@@ -2,24 +2,26 @@ import { Flow } from '@flows-ai/ui'
 import s from 'dedent'
 import { useEffect, useState } from 'react'
 
-const defaultCode = s`
+const codeWrapping = (flow: string) => s`
   import { parallel } from 'https://esm.sh/flows-ai/flows';
 
-  export default parallel([
+  export default ${flow}
+`
+
+const initialFlow = s`parallel([
     {
       agent: 'githubAgent',
       input: 'Get me the latest commit message from the main branch of the flows-ai repo',
     }
-  ])
-`
+  ])`
 
 export function Sandbox() {
-  const [code, setCode] = useState(defaultCode)
+  const [code, setCode] = useState(initialFlow)
   const [result, setResult] = useState<any>(null)
 
   const evaluateCode = async () => {
     try {
-      const blob = new Blob([code], { type: 'text/javascript' })
+      const blob = new Blob([codeWrapping(code)], { type: 'text/javascript' })
       const url = URL.createObjectURL(blob)
       /* vite-ignore */
       const module = await import(/* @vite-ignore */ url)
@@ -37,18 +39,19 @@ export function Sandbox() {
   }, [])
 
   return (
-    <div className="grid grid-cols-2 gap-4 not-content">
-      <div>
-        <textarea value={code} onChange={(e) => setCode(e.target.value)} onBlur={evaluateCode} />
+    <div className="grid grid-cols-2 gap-2 min-h-[50%]">
+      <div className="grid grid-rows-2 gap-2 col-span-1">
+        <textarea
+          className="w-full"
+          value={code}
+          onChange={(e) => setCode(e.target.value)}
+          onBlur={evaluateCode}
+        />
         <div>
           <pre className="whitespace-pre-wrap">{JSON.stringify(result, null, 2)}</pre>
         </div>
       </div>
-      {result && (
-        <div style={{ width: 600, height: 600 }}>
-          <Flow flow={result} />
-        </div>
-      )}
+      <div className="w-full h-full not-content">{result && <Flow flow={result} />}</div>
     </div>
   )
 }
