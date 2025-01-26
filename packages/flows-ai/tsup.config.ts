d40cafd7ddc9df24c113f890d59e16a55e559700
Mike Grabowski <grabbou@gmail.com>
2025-01-26 11:09:01 +0100

    fix: do not build tests

--- packages/flows-ai/tsup.config.ts
+++ packages/flows-ai/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/**/*.ts'],
+  entry: ['./src/**/!(*test).ts'],
   format: ['cjs', 'esm'],
   target: 'node20',
   splitting: false,
